91667e864ba19267c4d6040cb29098c517a3fbe8
pratibha.h <pratibha.h@innovify.in>
2020-12-23 15:56:07 +0530

    heroku changes

--- server.js
+++ server.js
@@ -9,5 +9,6 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static('./client/build'))
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, './', 'client', 'build', 'index.html'));
+
     });
 }
